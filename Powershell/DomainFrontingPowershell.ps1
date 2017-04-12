a1c780c74b2eab896c02d44dbe2eaaac773578f0
Eric Coldwell <coldwelle@users.noreply.github.com>
2017-04-12 11:44:34 -0500

    Now with SSL

--- Powershell/DomainFrontingPowershell.ps1
+++ Powershell/DomainFrontingPowershell.ps1
@@ -3,7 +3,6 @@
 #Unfortunatly WebClient is a bust because the host header is considered restricted
 #^This restrition comes from the WebHeaderCollection class so the WebRequest classes are out as well because that's how they all implement headers.
 
-Set-StrictMode -Version 2.0
 
 function DomainFront([string] $front, [string] $redirector, [int] $port, [string] $get)
 {
@@ -13,25 +12,24 @@ function DomainFront([string] $front, [string] $redirector, [int] $port, [string
     #Need to figure out why this is neccessary as the above is the same type with same value. Possibly a space being appended to "IPAddressToString" field?
     $frontIPAddress = [System.Net.IPAddress]::Parse($frontIPAddress[0])
     $frontIPEndPoint = [System.Net.IPEndPoint]::new($frontIPAddress, $port)
-    
+
     $sock = [System.Net.Sockets.Socket]::new([System.Net.Sockets.AddressFamily]::InterNetwork, [System.Net.Sockets.SocketType]::Stream, [System.Net.Sockets.ProtocolType]::Tcp)
     $sock.Connect($frontIPEndPoint)
 
-    #Powershell, as I have just discovered uses `n instead of \n
+    $networkStream = [System.Net.Sockets.NetworkStream]::new($sock)
+
+    $sslStream = [System.Net.Security.SslStream]::new($networkStream, $false)
+
+    $sslStream.AuthenticateAsClient($front)
+    
+    $buffer = New-Object System.Byte[] 1024
+
     $getString = "GET " + $get + " HTTP/1.1`r`nHost: " + $redirector + "`r`nContent-Length: 0`r`nConnection: Close`r`n`r`n"
     $byteGetString = [System.Text.Encoding]::ASCII.GetBytes($getString)
-    
-    $byteReceive = New-Object System.Byte[] 1024
 
-    $sock.Send($byteGetString, $byteGetString.Length, 0)
+    $sslStream.Write($byteGetString, 0, $byteGetString.length)
 
-    $byte = $sock.Receive($byteReceive, $byteReceive.Length, 0)
-    $strPage = [System.Text.Encoding]::ASCII.GetString($byteReceive, 0, $byte)
-
-    while($byte -gt 0){
-        $byte = $sock.Receive($byteReceive, $byteReceive.Length, 0)
-        $strPage = $strPage + [System.Text.Encoding]::ASCII.GetString($byteReceive, 0, $byte)
-    }
-    
-    Write-Host $strPage
+    $rawResponse = $sslStream.Read($buffer, 0, 1024)
+    $response = [System.Text.Encoding]::ASCII.GetString($buffer, 0, $rawResponse)
+    Write-Host $response
 }
